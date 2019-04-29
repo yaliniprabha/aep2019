9f2cb1a108cfc4b48165a6d66e15be6142a9302a
yaliniprabha <yalini.prabha@berkeley.edu>
2019-04-29 03:58:59 -0700

    Update PointTest.java

--- test/edu/berkeley/aep/PointTest.java
+++ test/edu/berkeley/aep/PointTest.java
@@ -31,4 +31,17 @@ public class PointTest {
         assertEquals(0.0, pointA.calc_euc_dist(pointA), 0.1);
     }
     
+     @Test
+    public void eucDistanceBetweenPointAtFourTwoAndItselfShouldBeZero() {
+        var pointA = new Point(4, 2);
+        assertEquals(0.0, pointA.calc_euc_dist(pointA), 0.1);
+    }
+    
+     @Test
+    public void eucDistanceBetweenPointAtFourThreeAndThreeOneShouldBeZero() {
+        var pointA = new Point(4, 3);
+        var pointB = new Point(3, 1);
+        assertEquals(Math.sqrt(5), pointA.calc_euc_dist(pointA), 0.1);
+    }
+    
 }
