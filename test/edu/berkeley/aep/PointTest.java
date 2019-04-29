1a7a54e33de8efd034e18818a6948fb1e854c726
yaliniprabha <yalini.prabha@berkeley.edu>
2019-04-28 22:53:15 -0700

    Update PointTest.java

--- test/edu/berkeley/aep/PointTest.java
+++ test/edu/berkeley/aep/PointTest.java
@@ -10,4 +10,12 @@ public class PointTest {
         var pointA = new Point(1);
         assertEquals(0, pointA.calc_dist(pointA));
     }
+    
+    @Test
+    public void oneDDistanceBetweenPointAtFourAndPointAtTenShouldBeSix() {
+        var pointA = new Point(4);
+        var pointB = new Point(10);
+        assertEquals(6, pointA.calc_dist(pointB));
+    }
+    
 }
