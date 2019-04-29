170ef3adb914e0a32683206846dfb9b33502e2ed
yaliniprabha <yalini.prabha@berkeley.edu>
2019-04-29 00:59:01 -0700

    Update Point.java

--- src/edu/berkeley/aep/Point.java
+++ src/edu/berkeley/aep/Point.java
@@ -6,13 +6,25 @@ import java.util.Map;
 // Understands a position on a numerical plane
 public class Point {
 
-    private final int position;
+    private final int xPosition;
+    private final int yPosition;
+   
      
-    public Point(int pos) {
-        this.position = pos;
+    public Point(int xpos) {
+        this.xPosition = xpos;
+    }
+    
+    public Point(int xpos, int ypos) {
+        this.xPosition = xpos;
+        this.yPosition = ypos;
     }
 
     public int calc_dist(Point other) {
         return Math.abs(this.position - other.position);
+    }
+    
+    public int calc_euc_dist(Point other) {
+        return Math.sqrt(Math.pow((this.xPosition - other.xPosition), 2) + Math.pow((this.xPosition - other.xPosition), 2));
+        
     }
 }
